6967d025c9c9517114402b1936368f3b2509eed7
MeAkash77 <itsaakash77@gmail.com>
2025-09-19 12:45:24 +0530

    first commit

---
+++ Backend/scripts/createAdmin.ts
@@ -0,0 +1,42 @@
+// scripts/createAdmin.ts
+import 'dotenv/config';
+import bcrypt from 'bcrypt';
+import prisma from '../src/shared/prisma';
+
+async function main() {
+  const phone = process.env.ADMIN_PHONE || '01710000000';
+  const rawPassword = process.env.ADMIN_PASS || 'Admin@123';
+  const saltRounds = Number(process.env.BCRYPT_SALT_ROUNDS || 10);
+
+  // Hash password
+  const hashed = await bcrypt.hash(rawPassword, saltRounds);
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        firstName: 'Admin',
+        lastName: 'User',
+        nationalId: process.env.ADMIN_NID || '0000000000', // must be unique
+        phoneNumber: phone, // must be unique
+        password: hashed,
+        pin: process.env.ADMIN_PIN || '1234',
+        role: 'admin',
+        isEmployee: true,
+      },
+    });
+
+    console.log('✅ Admin created:');
+    console.log({
+      id: user.id,
+      phoneNumber: user.phoneNumber,
+      nationalId: user.nationalId,
+      password: rawPassword,
+    });
+  } catch (err: any) {
+    console.error('❌ Error creating admin:', err.message || err);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main();
---
+++ src/constants/message.tsx
@@ -0,0 +1,30 @@
+const messages = [
+    {
+      id: 1,
+      sender: 'My Bank',
+      subject: 'Welcome to My Bank!',
+      message: 'Thank you for choosing My Bank. We are excited to have you on board!',
+      timestamp: '2024-01-01T09:00:00Z',
+      isRead: false,
+    },
+    {
+      id: 2,
+      sender: 'My Bank Support',
+      subject: 'Important Account Update',
+      message: 'Please review and update your account information to enjoy uninterrupted services.',
+      timestamp: '2024-01-08T14:30:00Z',
+      isRead: false,
+    },
+    {
+      id: 3,
+      sender: 'My Bank Offers',
+      subject: 'Exclusive Promotions Just for You!',
+      message: 'Explore our latest promotions and discounts tailored for you. Don’t miss out!',
+      timestamp: '2024-01-12T11:45:00Z',
+      isRead: false,
+    },
+    // Add more messages as needed
+  ];
+  
+  export default messages;
+  
